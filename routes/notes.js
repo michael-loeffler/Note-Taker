633864d8876c6eacc30735b716e0783a7b419155
mloeff <michaelloeffler23@gmail.com>
2023-02-11 21:17:13 -0600

    adds delete route to notes.js and adjusts the messages logged to the res and console

--- routes/notes.js
+++ routes/notes.js
@@ -8,12 +8,10 @@ const {
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-})
+});
 
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
-
     const { title, text } = req.body;
 
     if (req.body) {
@@ -24,10 +22,25 @@ notes.post('/', (req, res) => {
         };
 
         readAndAppend(newNote, './db/db.json');
-        res.json('Note added successfully');        
+        res.json(`Your note titled '${title}' has been added successfully.`);    
+        console.log(`Your note titled "${title}" has been added successfully.`)    
     } else {
         res.error('Error in adding note');
     }
+});
+
+notes.delete('/:id', (req, res) => {
+    const noteID = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((notes) => {
+            const newNotesDB = notes.filter((note) => note.id !== noteID);
+
+            writeToFile('./db/db.json', newNotesDB);
+
+            res.json(`Your note has been deleted.`);
+            console.log(`Your note has been deleted.`);
+        });
 });
 
 module.exports = notes;
